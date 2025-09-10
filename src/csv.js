9b134013f5c50f2aa2492d1b5f09723c9e544eb2
Austin Slater <aslater@waldoch.com>
2025-09-10 17:05:19 -0500

    correctly processes csv

--- src/csv.js
+++ src/csv.js
@@ -4,32 +4,32 @@ import { createObjectCsvWriter } from "csv-writer";
 import { INPUT_FILE, OUTPUT_FILE } from "./config.js";
 
 export function readPartNumbers() {
-  return new Promise((resolve) => {
-    const partNumbers = [];
-    fs.createReadStream(INPUT_FILE)
-      .pipe(csv())
-      .on("headers", (headers) => console.log("CSV headers:", headers))
-      .on("data", (row) => {
-        // Debug
-        console.log("Row data:", row);
-        
-        if (row["Item Name/Number"]) {
-          partNumbers.push(row["Item Name/Number"].trim());
-        }
-      })
-      .on("end", () => resolve(partNumbers));
-  });
+    return new Promise((resolve) => {
+        const partNumbers = [];
+        fs.createReadStream(INPUT_FILE)
+            .pipe(
+                csv({
+                    mapHeaders: ({ header }) => header.trim(), // normalize headers
+                })
+            )
+            .on("data", (row) => {
+                if (row["Item Name/Number"]) {
+                    partNumbers.push(row["Item Name/Number"].trim());
+                }
+            })
+            .on("end", () => resolve(partNumbers));
+    });
 }
 
 export async function writeResults(results) {
-  const csvWriter = createObjectCsvWriter({
-    path: OUTPUT_FILE,
-    header: [
-      { id: "sku", title: "SKU" },
-      { id: "shopifyUrl", title: "Shopify URL" },
-      { id: "waybackUrl", title: "Wayback Snapshot" },
-    ],
-  });
+    const csvWriter = createObjectCsvWriter({
+        path: OUTPUT_FILE,
+        header: [
+            { id: "sku", title: "SKU" },
+            { id: "shopifyUrl", title: "Shopify URL" },
+            { id: "waybackUrl", title: "Wayback Snapshot" },
+        ],
+    });
 
-  await csvWriter.writeRecords(results);
+    await csvWriter.writeRecords(results);
 }
