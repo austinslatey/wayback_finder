9caf97b9581ac934505161c4663df10884dc2a09
Austin Slater <aslater@waldoch.com>
2025-09-10 16:30:11 -0500

    configuration variables for other files to use freely

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const SHOPIFY_DOMAIN = "waldoch-truck-accessories-store.myshopify.com";
+export const ADMIN_API_VERSION = "2025-01";
+export const ADMIN_TOKEN = process.env.ADMIN_TOKEN
+export const STORE_FRONT_URL = "https://store.waldoch.com/products";
+
+// File paths
+export const INPUT_FILE = "parts.csv";
+export const OUTPUT_FILE = "wayback_results.csv";
