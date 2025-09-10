0bbe7e1f2b598c0eb834328be661d79bc7dbcc58
Austin Slater <aslater@waldoch.com>
2025-09-10 16:31:25 -0500

    handles piping of CSV input/output

---
+++ src/csv.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import csv from "csv-parser";
+import { createObjectCsvWriter } from "csv-writer";
+import { INPUT_FILE, OUTPUT_FILE } from "./config.js";
+
+export function readPartNumbers() {
+  return new Promise((resolve) => {
+    const partNumbers = [];
+    fs.createReadStream(INPUT_FILE)
+      .pipe(csv())
+      .on("data", (row) => {
+        if (row["Item Name/Number"]) {
+          partNumbers.push(row["Item Name/Number"].trim());
+        }
+      })
+      .on("end", () => resolve(partNumbers));
+  });
+}
+
+export async function writeResults(results) {
+  const csvWriter = createObjectCsvWriter({
+    path: OUTPUT_FILE,
+    header: [
+      { id: "sku", title: "SKU" },
+      { id: "shopifyUrl", title: "Shopify URL" },
+      { id: "waybackUrl", title: "Wayback Snapshot" },
+    ],
+  });
+
+  await csvWriter.writeRecords(results);
+}
